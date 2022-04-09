07e4ad31de592f0c400d6782296ebf343972b960
tomADC443 <tom@tpl.dev>
2022-04-09 16:09:42 +0200

    feat: env config

---
+++ config.ts
@@ -0,0 +1,20 @@
+const getEnvironmentVariable = (environmentVariable: string): string => {
+  const unvalidatedEnvironmentVariable = process.env[environmentVariable];
+  if (!unvalidatedEnvironmentVariable) {
+    throw new Error(
+      `Couldn't find environment variable: ${environmentVariable}`
+    );
+  } else {
+    return unvalidatedEnvironmentVariable;
+  }
+};
+
+const config = {
+  DATABASE_URL: getEnvironmentVariable("DATABASE_URL"),
+  SHADOW_DATABASE_URL: getEnvironmentVariable("SHADOW_DATABASE_URL"),
+  GOOGLE_CLIENT_ID: getEnvironmentVariable("GOOGLE_CLIENT_ID"),
+  GOOGLE_CLIENT_SECRET: getEnvironmentVariable("GOOGLE_CLIENT_SECRET"),
+  JWT_SECRET: getEnvironmentVariable("JWT_SECRET"),
+};
+
+export default config;
