554b1eca803e700e71cbfe9a6e95b88cac642b59
tomADC443 <tom@tpl.dev>
2022-04-30 23:34:13 +0200

    styles: navbar and quiz

--- components/Quiz/Answers.tsx
+++ components/Quiz/Answers.tsx
@@ -1,20 +1,21 @@
-import { MouseEvent } from "react";
 import { Answer, handleAnswerClick } from "../../pages/quiz";
 
 interface AnswerProps {
-  handleClick:  handleAnswerClick;
+  handleClick: handleAnswerClick;
   answers: Array<Answer>;
 }
 
 const Answers = (props: AnswerProps) => {
-  const answerButtons = props.answers.map((answer) => {
-    return (
-      <button onClick={()=>props.handleClick(answer.id)} key={answer.id}>
-        {answer.phrase}{" "}
-      </button>
-    );
-  });
+  const answerButtons = props.answers.map((answer) => (
+    <button
+      onClick={() => props.handleClick(answer.id)}
+      key={answer.id}
+      className="button quiz-answer"
+    >
+      {answer.phrase}
+    </button>
+  ));
 
-  return <div>{answerButtons}</div>;
+  return <div className="quiz-answers-grid">{answerButtons}</div>;
 };
 export default Answers;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
 import { NextPage } from "next";
 import Navbar from "../components/Navbar/Navbar";
-import { useSession } from "next-auth/react"
-import Link from 'next/link'
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Landing: NextPage = () => {
-  const { data: session, status } = useSession()
-  const loading = status === 'loading'
+  const { data: session, status } = useSession();
 
   return (
-    <div>
-      <Navbar></Navbar>
-      { <h1>Hi {session?.user?.given_name || "Stranger"}</h1> }
-      <h2>
-        Click < Link href="/quiz">here</Link> to learn some http codes.
-      </h2>
-    </div>
+    <>
+      <Navbar />
+      <div className="content">
+        <h1>Hi {session?.user?.given_name || "Stranger"}</h1>
+        <h2>
+          Click <Link href="/quiz">here</Link> to learn some http codes.
+        </h2>
+      </div>
+    </>
   );
 };
-
 export default Landing;
